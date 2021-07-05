259d55a16c5be6eb1e1fd133f36fb6da0f1cc34b
Radoslaw Medryk <rame@trustpilot.com>
2021-07-05 23:03:59 +0200

    Added 3rd test, green

--- CaptainSkuEngineUnitTests/CombinationPromotionEngineTests.cs
+++ CaptainSkuEngineUnitTests/CombinationPromotionEngineTests.cs
@@ -168,6 +168,106 @@ namespace CaptainSkuEngineUnitTests
                     }
                 }, result);
         }
+        
+        [Test]
+        public void AppliesMultiSkuPromotions()
+        {
+            var engine = CreateTestEngine();
+
+            var result = engine.ApplyPromotion(new List<SkuWithCount>
+            {
+                new SkuWithCount
+                {
+                    Sku = TestSkuA,
+                    Count = 3
+                },
+                new SkuWithCount
+                {
+                    Sku = TestSkuB,
+                    Count = 5
+                },
+                new SkuWithCount
+                {
+                    Sku = TestSkuC,
+                    Count = 1
+                },
+                new SkuWithCount
+                {
+                    Sku = TestSkuD,
+                    Count = 1
+                },
+            });
+            
+            Helpers.AssertJsonEqual( 
+                new PromotionResult
+                {
+                    TotalPrice = 280,
+                    PromotionalGroups = new List<PricedGroup>
+                    {
+                        new PricedGroup
+                        {
+                            TotalPrice = 130,
+                            Entries = new List<SkuWithCount>
+                            {
+                                new SkuWithCount
+                                {
+                                    Sku = TestSkuA,
+                                    Count = 3
+                                }
+                            }
+                        },
+                        new PricedGroup
+                        {
+                            TotalPrice = 45,
+                            Entries = new List<SkuWithCount>
+                            {
+                                new SkuWithCount
+                                {
+                                    Sku = TestSkuB,
+                                    Count = 2
+                                }
+                            }
+                        },
+                        new PricedGroup
+                        {
+                            TotalPrice = 45,
+                            Entries = new List<SkuWithCount>
+                            {
+                                new SkuWithCount
+                                {
+                                    Sku = TestSkuB,
+                                    Count = 2
+                                }
+                            }
+                        },
+                        new PricedGroup
+                        {
+                            TotalPrice = 30,
+                            Entries = new List<SkuWithCount>
+                            {
+                                new SkuWithCount
+                                {
+                                    Sku = TestSkuC,
+                                    Count = 1
+                                },
+                                new SkuWithCount
+                                {
+                                    Sku = TestSkuD,
+                                    Count = 1
+                                }
+                            }
+                        }
+                    },
+                    OmittedEntries = new []
+                    {
+                        new SkuWithCount
+                        {
+                            Sku = TestSkuB,
+                            Count = 1
+                        },
+                    }
+                }, result);
+        }
 
         private PromotionEngine CreateTestEngine()
         {
